4e7b8f8c7d668012cc4dac947e64354c3c699cca
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-11-13 20:37:33 +0530

    feat: add theme switch dropdown

--- src/components/CodePlayground.tsx
+++ src/components/CodePlayground.tsx
@@ -1,8 +1,18 @@
 import CodeEditorGroup from "./CodeEditorGroup";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Cloud } from "lucide-react";
+import { Cloud, Settings } from "lucide-react";
 import { ModeToggle } from "./ModeToggle";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Label } from "@/components/ui/label";
+
+// TODO: penweave logo, filename and username
+// TODO: font and editor theme select
+// TODO: https://medium.com/@yuvrajkakkar1/best-2024-top-20-visual-studio-code-extensions-for-react-js-developers-f3bfde74d4e2
 
 function CodePlayground() {
   return (
@@ -12,6 +22,19 @@ function CodePlayground() {
           <strong>Pen Weave</strong>
         </div>
         <div className="flex gap-2">
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="outline" size="icon">
+                <Settings />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <Label htmlFor="font-select">Select an Editor font</Label>
+              <Label htmlFor="editor-theme-select">
+                Select an Editor Theme
+              </Label>
+            </PopoverContent>
+          </Popover>
           <ModeToggle />
           <Button
             variant="outline"
