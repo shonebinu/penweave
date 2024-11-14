c364ea45bb0df05096d88e8663a96d0876be70a5
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-11-14 10:23:29 +0530

    feat: implement context for editor code values

---
+++ src/contexts/CodeContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState } from "react";
+
+const CODE_DEFAULT_VALUES = {
+  html: `<button>Click Here</button>`,
+  css: `button {
+    padding: 10px;
+}`,
+  js: `document.querySelector("button").onClick = () => window.alert("Welcome to Pen Weave");`,
+};
+
+type CodeState = {
+  htmlCode: string;
+  cssCode: string;
+  jsCode: string;
+  setHtmlCode: (code: string) => void;
+  setCssCode: (code: string) => void;
+  setJsCode: (code: string) => void;
+};
+
+export const CodeContext = createContext<CodeState | undefined>(undefined);
+
+export function CodeProvider({ children }: { children: React.ReactNode }) {
+  const [htmlCode, setHtmlCode] = useState(CODE_DEFAULT_VALUES.html);
+  const [cssCode, setCssCode] = useState(CODE_DEFAULT_VALUES.css);
+  const [jsCode, setJsCode] = useState(CODE_DEFAULT_VALUES.js);
+
+  return (
+    <CodeContext.Provider
+      value={{
+        htmlCode,
+        cssCode,
+        jsCode,
+        setHtmlCode,
+        setCssCode,
+        setJsCode,
+      }}
+    >
+      {children}
+    </CodeContext.Provider>
+  );
+}
+
+export function useCode() {
+  const context = useContext(CodeContext);
+  if (context === undefined) {
+    throw new Error("useCode must be used within a CodeProvider");
+  }
+  return context;
+}
