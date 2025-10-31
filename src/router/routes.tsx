66a955bd2729e3187ab9b852aa4f6ceb056850f9
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-10-31 11:28:48 +0530

    feature reorg

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -6,8 +6,8 @@ import Logout from "@/features/auth/pages/Logout.tsx";
 import ResetPassword from "@/features/auth/pages/ResetPassword.tsx";
 import Signup from "@/features/auth/pages/Signup.tsx";
 import VerifyEmail from "@/features/auth/pages/VerifyEmail.tsx";
-import Editor from "@/features/editor/pages/Editor.tsx";
-import Projects from "@/features/projects/pages/Projects.tsx";
+import Editor from "@/features/projects/pages/Editor.tsx";
+import Projects from "@/features/tmp/pages/Projects.tsx";
 import AuthLayout from "@/layouts/AuthLayout.tsx";
 import MainLayout from "@/layouts/MainLayout.tsx";
 
