1065af310968350f7617b21fe0a812d7f9d2c22d
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-11-02 15:20:04 +0530

    use daisy ui tabs instead of react-tabs

--- src/features/projects/components/EditorTabs.tsx
+++ src/features/projects/components/EditorTabs.tsx
@@ -1,7 +1,3 @@
-import { Code, PaintRoller, Parentheses } from "lucide-react";
-
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
-
 import { css } from "@codemirror/lang-css";
 import { html } from "@codemirror/lang-html";
 import { javascript } from "@codemirror/lang-javascript";
@@ -23,27 +19,21 @@ export default function EditorTabs({
   setCssCode: (value: string) => void;
   setJsCode: (value: string) => void;
 }) {
-  const tabButtonStyles =
-    "text-base-content hover:bg-base-200 flex w-full items-center justify-center rounded-md font-medium outline-none hover:cursor-pointer";
-
   const tabs = [
     {
       label: "HTML",
-      icon: <Code size="1rem" className="text-error mr-2" />,
       value: htmlCode,
       setValue: setHtmlCode,
       extensions: [html()],
     },
     {
       label: "CSS",
-      icon: <PaintRoller size="1rem" className="text-info mr-2" />,
       value: cssCode,
       setValue: setCssCode,
       extensions: [css()],
     },
     {
       label: "JS",
-      icon: <Parentheses size="1rem" className="text-warning mr-2" />,
       value: jsCode,
       setValue: setJsCode,
       extensions: [javascript()],
@@ -51,25 +41,27 @@ export default function EditorTabs({
   ];
 
   return (
-    <Tabs selectedTabClassName="bg-base-300">
-      <TabList className="flex h-10 w-full gap-1">
-        {tabs.map(({ label, icon }) => (
-          <Tab key={label} className={tabButtonStyles}>
-            {icon} {label}
-          </Tab>
-        ))}
-      </TabList>
+    <div className="tabs tabs-border">
       {tabs.map(({ label, value, setValue, extensions }) => (
-        <TabPanel key={label}>
-          <div className="h-[40svh]">
-            <CodePanel
-              value={value}
-              extensions={extensions}
-              onChange={(val) => setValue(val)}
-            />
+        <>
+          <input
+            type="radio"
+            name="editor-tabs"
+            className="tab flex-1 font-medium"
+            aria-label={label}
+            defaultChecked={label === "HTML"}
+          />
+          <div className="tab-content bg-base-100">
+            <div className="h-[40svh]">
+              <CodePanel
+                value={value}
+                extensions={extensions}
+                onChange={(val) => setValue(val)}
+              />
+            </div>
           </div>
-        </TabPanel>
+        </>
       ))}
-    </Tabs>
+    </div>
   );
 }
