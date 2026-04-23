9658dbee56fe8a47b5b1e812a35343042796fd9f
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-04-23 19:35:28 +0530

    move toast to bottom

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
       <RouterProvider router={router} />
-      <Toaster position="top-center" />
+      <Toaster position="bottom-center" />
     </AuthContextProvider>
   </StrictMode>,
 );
